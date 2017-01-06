73e2dfed05b34f1332f6b5f620c324a480c616ca
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-01-06 15:04:24 -0800

    change from production to staging

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ require('dotenv').load();
 var Classy = require('classy-node');
 
 var classy = new Classy({
+	baseUrl: 'https://stagingapi.stayclassy.org',
 	clientId: process.env.CLIENT_ID,
 	clientSecret: process.env.CLIENT_SECRET
 });
 
 classy.app().then(function() {
 	console.log("test");
-})
+});
