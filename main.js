66ea03a8c9a2b2932617ed77ade498439b0217c6
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-01-09 15:17:37 -0800

    loop through all transactions for org 34, return JSON response

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 require('dotenv').load();
 
 var Classy = require('classy-node');
+var fs = require('fs');
+var csv = require('fast-csv');
 
 var classy = new Classy({
 	baseUrl: 'https://stagingapi.stayclassy.org',
@@ -13,12 +15,15 @@ const app = classy.app();
 
 app.then(function() {
 
-	classy.campaigns.retrieve(72, {
-		token: 'app'
+	// First, loop through all transactions (with sample time filter)
+	classy.organizations.listTransactions(34, {
+		token: 'app',
+		filter: 'purchased_at>2017-01-01T10:00:00'
 	})
 
 	.then(function(response) {
-		console.log("RESPONSE: " + response.id);
+		// response should be an array of JSON transaction data
+    console.log("RESPONSE: ", response);
 	})
 
 	.catch(function(error) {
@@ -26,3 +31,16 @@ app.then(function() {
 	});
 
 });
+
+// var ws = fs.createWriteStream('./test.csv');
+// // !! How to save file to user's desktop instead?
+
+// csv
+// 	.write([
+// 		["a1", "b1"],
+// 		["b2", "c2"],
+// 		["c2", "d2"],
+// 		["d"]
+// 		], {headers:true})
+// 	.pipe(ws);
+
