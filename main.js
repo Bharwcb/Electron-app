6b2280c4d7b42879a2c7a7b4c95791e7b7b16dc2
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-01-10 17:47:41 -0800

    adding with errors, looking into further tomor

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ var classy = new Classy({
 const app = classy.app();
 
 // one place to change headers for import
-const headers = ["Contact ID", "Title"];
+const csvHeaders = ["Contact ID", "Title"];
 
 // used to collect list of contact IDs, title, or whatever you are fetching. then, write to csv.
-let classy_data = [];
+let classyData = [];
 
 app.then(function() {
 
@@ -28,22 +28,66 @@ app.then(function() {
 	})
 
 	.then(function(response) {
-		// response is an array of JSON transaction data, 20 per page
-
-    var ws = fs.createWriteStream('./result.csv');
-
+		// response is an array of JSON transaction data, 20 per page. Add the first page of responses to var classyData.
+		var ws = fs.createWriteStream('./result.csv');
+		console.log("PAGE 1");
 		for (i = 0; i < response.data.length; i++) {
-			// formatting in the way fast-csv wants it to put each data point in a new row.. [[h1,h1], [r1,c1], [r2,c2]]
-			classy_data.push(new Array(response.data[i].member_id.toString()));
+			console.log("I: ", i);
+			console.log("TRANSACTION_ID: ", response.data[i].id);
+			// formatting var classyData in the way fast-csv wants it to put each of 20 transactions row by row.. [[h1,h1], [r1,c1], [r2,c2]].
+			classyData.push(new Array(response.data[i].member_id.toString()));
 		}
 
+		// When done with first page, find out how many pages total were in the response..
+		const numberOfPages = response.last_page;
+		let promises = [];
+
+		// And request all remaining pages after the first page.
+		// Add them to promises array to call all asynchronously
+		// PROMISE IS GETTING ADDED, BUT NONE OF .THEN(FUNCTION(HIGHERPAGERESPONSES) IS RUNNING)
+		for (page = 2; page < (numberOfPages + 1); page++) {
+			console.log("PAGE# ", page);
+			promises.push(
+				function() {
+					classy.organizations.listTransactions(34, {
+						token: 'app',
+						filter: 'purchased_at>2017-01-01T10:00:00',
+						page: page
+					})
+
+					// Add each page's 20 transactions to var classyData. higherPagesResponses is just the 20 transactions per page above 2.
+					.then(function(higherPagesResponses) {
+						for (x = 0; x < higherPagesResponses.data.length; x++) {
+
+							console.log("X: ", x);
+							console.log("TRANSACTION_ID: ", higherPagesResponses.data[i].id);
+							// formatting in the way fast-csv wants it to put each data point in a new row.. [[h1,h1], [r1,c1], [r2,c2]].
+							classyData.push(new Array(higherPagesResponses.data[i].member_id.toString()));
+						}
+					});
+				}
+			)
+			console.log("PROMISES LENGTH: ", promises.length);
+		};
+
+		// And request them all asynchronously
+		Promise.all(promises).then((results) => {
+			// ~~~~~ MIGHT NEED TO FLATTEN ARRAY HERE!  ~~~~~ see what this looks like
+	    // but the results should be an array of all data return from pages 2 through 134.
+		})
+
+		.catch((err) => {
+			console.log("ERROR 2ND THROUGH LAST PAGE: " + error);
+	  });
+		
+		// LASTLY, PIPE CLASSYDATA INTO CSV
 		csv
-			.write( classy_data, {headers: headers} )
+			.write( classyData, {headers: csvHeaders} )
 			.pipe(ws);
 	})
 
 	.catch(function(error) {
-		console.log("ERROR: " + error);
+		console.log("ERROR ON FIRST PAGE: " + error);
 	});
 
 });
