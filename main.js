fdc74dd682c94d2766938cf08453d5190effe305
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-01-19 18:54:53 -0800

    cleaned up code, ready to start on remaining custom questions

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var attributes = require('./attributes');
 var async = require('async');
 var classy = require('./classy-build');
 const app = classy.app();
-
-const time_filter = '>2017-01-18T10:00:00';
+var ws = fs.createWriteStream('./result.csv');
+const time_filter = '>2017-01-19T10:00:00';
 
 // one place to change headers for import
 const csvHeaders = ["Contact ID", "Title", "Last Name", "First Name", "Middle Name", "Company", "Suffix", "Billing Email", "Phone", "Street 1", "Street 2", "City", "State/Providence", "ZIP/Postal Code", "Country", "Member ID", "Campaign Title", "Form Title", "Net Transaction Amount", "Transaction Date", "Gift Type", "Temple Name", "Designee 1 Administrative Name", "Origin of Gift", "Payment Method", "Settlement Status", "Billing Last Name", "Billing First Name", "Billing Middle Name", "Billing Suffix", "Billing Street1", "Billing Street2", "Billing City", "Billing State", "Billing Zip", "Billing Phone", "Is Honor Gift", "Tribute First Name", "Tribute Last Name", "Sender Title", "Sender First Name", "Sender Last Name", "Sender Address 1", "Sender Address 2", "Sender City", "Sender State", "Sender Zip", "Sender Country", "Source Code Type", "Source Code Text", "Sub Source Code Text", "Name of Staff Member", "Donation Comment", "Store Name"];
@@ -18,17 +18,11 @@ let classyData = [];
 // used for custom answers, to avoid querying API more than have to.
 let indexedTitle = {};
 
-var ws = fs.createWriteStream('./result.csv');
-
-
 app
 .then(() => {
 	return require('./title')(indexedTitle, time_filter);
 })
 .then(() => {
-
-	console.log("indexed title: ", indexedTitle);
-	
 	// First, loop through all transactions (with sample time filter)
 	return classy.organizations.listTransactions(34, {
 		token: 'app',
--- title.js
+++ title.js
@@ -1,13 +1,13 @@
 /* 
 
-Lets make any request for custom answer a function, then have a separate file for populating the indexed Title hash.
+1st, build indexed custom title hash to avoid hitting custom questoins api for every transactions..  just do it once.
 
-So for example,
+then, when iterating through transactions, if doesn't find that transaction.id in customTitle hash, means they didn't enter it, doesn't set it
 
-/custom-answers directory
-- title.js:  uses it to build the indexedTitle hash
-- company.js: uses it to build the indexedCompany hash
-- maybe a reusable function to send the request to ALL CUSTOM QUESTIONS, just plugin the question_id, blank indexed hash, etc.
+{
+	12345: 'Mr.',
+	23456: 'Mrs.'
+}
 
 */
 
@@ -22,7 +22,6 @@ var buildCustomTitleHash = function(indexedTitle, time_filter) {
 	})
 	.then((answersResults) => {
 
-		console.log("page 1 answer results: ", answersResults);
 		let answers = answersResults.data;
 		answers.forEach(answer => {
 			indexedTitle[answer.answerable_id] = answer.answer;
@@ -44,26 +43,17 @@ var buildCustomTitleHash = function(indexedTitle, time_filter) {
 		
 		return Promise.all(titlePromises);
 	})
-	.then((response) => {
-		return response;
-	}, (err) => {
-		console.log('error earlier', err);
-	})
 	.then((titleResults) => {
-		console.log("Title results pages 2 through end: ", titleResults);
 		titleResults.forEach(function(arrayofTitlesPerPage) {
 
 			var arrayofTitles = arrayofTitlesPerPage.data;
 			arrayofTitles.forEach(function(answer, index) {
 				indexedTitle[answer.answerable_id] = answer.answer;
-			});
-			// so, it's building indexed title correctly.
+			})
+			
 		});
-	});
-	// .catch((error) => {
-	// 	console.log("ERROR IN ANSWERS OTHER PAGES", error);
-	// });
-	// // ~~ End of async step 1 (title).  SHOULD collect the whole indexedTitle by now.
+	})
+	
 };
 
 module.exports = buildCustomTitleHash;
