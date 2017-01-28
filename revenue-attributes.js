f383d8f1a3659f6b525c7a3ede00dc534eecc4ba
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-01-28 12:00:32 -0800

    title and suffix complete

--- revenue-attributes.js
+++ revenue-attributes.js
@@ -1,5 +1,5 @@
 module.exports = {
-	fetchAttributes: function(transaction, revenueData, indexedCompany, indexedMiddlename) {
+	fetchAttributes: function(transaction, revenueData, indexedCompany, indexedMiddlename, indexedTitle, indexedSuffix) {
 		
 		let transaction_id = transaction.id;
 
@@ -15,22 +15,17 @@ module.exports = {
 		let first_name = transaction.billing_first_name;
 		let last_name = transaction.billing_last_name;
 		let middle_name = indexedMiddlename[transaction_id];
+		let title = indexedTitle[transaction_id];
+		let suffix = indexedSuffix[transaction_id];
 		let full_name = [];
-		function nameExists(first_name, last_name) {
-			if ((first_name !== null) || (last_name !== null)) {
-				return true;
-			} else {
-				return false;
-			}
-		};
 		// if there's a company given, set last_org to company
 		if (( typeof company_name !== 'undefined' ) && ( company_name !== "" )) {
 			last_org = company_name;
 		} else {
 			// if no company name, set last_org to full name 
 			if (nameExists(first_name, last_name)) {
-				full_name.push(first_name, middle_name, last_name);
-				// if last name OR first name isn't given (only possible with transactions via the API), or for some strange reason only a middle name is given, use whatever is provided by filtering out nulls
+				full_name.push(title, first_name, middle_name, last_name, suffix);
+				// if last name OR first name isn't given (only possible with transactions via the API), or for some strange reason there's a first name, middle name, and suffix, but no last name, etc.. use whatever is provided by filtering out nulls
 				full_name.filter(function(val) { return val !== null; });
 				last_org = full_name.join(" ");
 			}
@@ -40,6 +35,14 @@ module.exports = {
 
 
 
-		revenueData.push([account_system, constituent, lookup_id, last_org, first_name, middle_name]);
+		revenueData.push([account_system, constituent, lookup_id, last_org, first_name, middle_name, title, suffix]);
+
+		function nameExists(first_name, last_name) {
+			if ((first_name !== null) || (last_name !== null)) {
+				return true;
+			} else {
+				return false;
+			}
+		};
 	}
 };
