86c684f103a1d29e9c847f3bdaaaabe3f8ab3b09
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-03-13 11:19:49 -0700

    about to implement filelist controller

---
+++ angularApp.js
@@ -0,0 +1,12 @@
+// // define module
+// var fileListApp = angular.module('fileListApp', []);
+
+// // define controller
+// fileListApp.controller('fileListController', function fileListController($scope) {
+// 	$scope.files = [
+// 		'file 1',
+// 		'file 2'
+// 	]
+// });
+
+console.log("LOAD?");
