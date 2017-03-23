514628dc7c784d11afd572193be08f7d3e4cac82
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-03-22 17:19:06 -0700

    start spinner works

--- main-controller.js
+++ main-controller.js
@@ -1,8 +1,7 @@
 var myApp = angular.module('mainApp', ['angularSpinner']);
 
-myApp.controller('mainController', mainController);
+myApp.controller('mainController', ['$scope', 'usSpinnerService', function($scope, usSpinnerService) {
 
-function mainController($scope) {
   // ~~~ display CSV titles in sidebar
   $scope.constituentCSVDisplaySidebar = window.constituentCSVDisplaySidebar;
   $scope.revenueCSVDisplaySidebar = window.revenueCSVDisplaySidebar;
@@ -10,23 +9,22 @@ function mainController($scope) {
     $scope.constituentCSVDisplaySidebar = window.constituentCSVDisplaySidebar;
     $scope.revenueCSVDisplaySidebar = window.revenueCSVDisplaySidebar;
   };
-	$scope.files = [];
+  $scope.files = [];
   // add any number of files in template
-	$scope.addFile = function(...toDisplay) {
+  $scope.addFile = function(...toDisplay) {
     for (var file of toDisplay) {
       $scope.files.push(file)
     }
-	};
+  };
   // ~~~
 
   // ~~~ spinner when generating report
-  // $scope.startSpin = function() {
-    
-  // }
+  $scope.startSpin = function() {
+    usSpinnerService.spin('spinner');
+  }
 
+}]);
 
-
-}
 
 // spinner settings
 myApp.config(['usSpinnerConfigProvider', function(usSpinnerConfigProvider) {
