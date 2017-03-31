4042d4762040821578a07b5eb27e5e11b6220180
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-03-30 17:07:36 -0700

    complete

--- classy-build.js
+++ classy-build.js
@@ -1,5 +1,6 @@
 var Classy = require('classy-node');
 
+// 'config' module since 'dotenv' does not work with electron
 var config = require('config');
 var id = config.get('classyCredentials.id');
 var secret = config.get('classyCredentials.secret');
