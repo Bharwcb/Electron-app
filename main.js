0057ceced2724be2dc76fea6726939019877138f
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-03-31 14:42:43 -0700

    new errros

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const path = require('path');
 const url = require('url');
 // require('dotenv').config();
 
+if (process.versions && process.versions.electron) {
+  process.env.NODE_CONFIG_DIR = require('electron').app.getAppPath() + '/config';
+}
+
 let mainWindow = null;
 
 // when electron initializes and is ready to create browser windows
