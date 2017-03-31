cc5192c5d3bb7e39fb8029425b08b9ecb140b17f
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-03-30 17:06:41 -0700

    complete

--- classy-build.js
+++ classy-build.js
@@ -2,9 +2,7 @@ var Classy = require('classy-node');
 
 var config = require('config');
 var id = config.get('classyCredentials.id');
-console.log("ID: ", id);
 var secret = config.get('classyCredentials.secret');
-console.log("secret: ", secret);
 
 var classy = new Classy({
 	baseUrl: 'https://stagingapi.stayclassy.org',
